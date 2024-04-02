d815934fe729c2226c192c90de9105b6043d62e8
yassineAchouyne <ACHOUYNE.YASSINE@ofppt-edu.ma>
2024-04-02 15:37:33 +0100

    Update navbar.blade.php

--- CODE/resources/views/layouts/navbar.blade.php
+++ CODE/resources/views/layouts/navbar.blade.php
@@ -24,8 +24,28 @@
                      <li class="nav-item">
                          <a class="nav-link" href="">Contact Us</a>
                      </li>
+                     <li class="nav-item">
+                         <a class="nav-link" data-toggle="modal" data-target="#exampleModalToggle">Login</a>
+                     </li>
                  </ul>
              </div>
          </div>
      </nav>
  </header>
+
+ <div class="modal fade" id="exampleModalToggle" aria-hidden="true" aria-labelledby="exampleModalToggleLabel" tabindex="-1">
+     <div class="modal-dialog modal-dialog-centered">
+         <div class="modal-content">
+             <div class="modal-header">
+                 <h1 class="modal-title fs-5" id="exampleModalToggleLabel">Modal 1</h1>
+                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+             </div>
+             <div class="modal-body">
+                 Show a second modal and hide this one with the button below.
+             </div>
+             <div class="modal-footer">
+                 <button class="btn btn-primary" data-bs-target="#exampleModalToggle2" data-bs-toggle="modal">Open second modal</button>
+             </div>
+         </div>
+     </div>
+ </div>
