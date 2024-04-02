625ccb20f71e0de6238284d30df564497f13bf06
yassineAchouyne <ACHOUYNE.YASSINE@ofppt-edu.ma>
2024-04-02 16:21:26 +0100

    add

--- CODE/resources/views/layouts/navbar.blade.php
+++ CODE/resources/views/layouts/navbar.blade.php
@@ -25,7 +25,7 @@
                          <a class="nav-link" href="">Contact Us</a>
                      </li>
                      <li class="nav-item">
-                         <a class="nav-link" data-toggle="modal" data-target="#exampleModalToggle">Login</a>
+                         <a class="nav-link" id="show">Login</a>
                      </li>
                  </ul>
              </div>
@@ -33,19 +33,154 @@
      </nav>
  </header>
 
- <div class="modal fade" id="exampleModalToggle" aria-hidden="true" aria-labelledby="exampleModalToggleLabel" tabindex="-1">
-     <div class="modal-dialog modal-dialog-centered">
-         <div class="modal-content">
-             <div class="modal-header">
-                 <h1 class="modal-title fs-5" id="exampleModalToggleLabel">Modal 1</h1>
-                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-             </div>
-             <div class="modal-body">
-                 Show a second modal and hide this one with the button below.
-             </div>
-             <div class="modal-footer">
-                 <button class="btn btn-primary" data-bs-target="#exampleModalToggle2" data-bs-toggle="modal">Open second modal</button>
-             </div>
+
+
+ <!-- Modal -->
+ <div class="model-tog">
+     <div class="main">
+         <input type="checkbox" id="chk" aria-hidden="true">
+
+         <div class="signup">
+             <form class="">
+                 <label for="chk" aria-hidden="true">Sign up</label>
+                 <input type="text" class="searchText" name="txt" placeholder="User name" required="">
+                 <input type="email" name="email" placeholder="Email" required="">
+                 <input type="password" name="pswd" placeholder="Password" required="">
+                 <button>Sign up</button>
+             </form>
          </div>
+
+         <div class="login">
+             <form>
+                 <label for="chk" aria-hidden="true">Login</label>
+                 <input type="email" name="email" placeholder="Email" required="">
+                 <input type="password" name="pswd" placeholder="Password" required="">
+                 <button>Login</button>
+             </form>
+         </div>
+         <img src="img/retirer.png" id="close" />
      </div>
  </div>
+
+
+
+ <style>
+     .model-tog {
+         position: fixed;
+         z-index: 9999;
+         display: flex;
+         justify-content: center;
+         align-items: center;
+         width: 100%;
+         height: 100vh;
+         display: none;
+     }
+
+     .model-tog img {
+         position: absolute;
+         top: 3px;
+         right: 3px;
+     }
+
+     .main {
+         width: 350px;
+         height: 500px;
+         background: red;
+         overflow: hidden;
+         background-color: #f7f7f7;
+         border-radius: 10px;
+         box-shadow: 5px 20px 50px #000;
+         position: relative;
+     }
+
+     #chk {
+         display: none;
+     }
+
+     .signup {
+         position: relative;
+         width: 100%;
+         height: 100%;
+     }
+
+     .main label {
+         color: #f48840;
+         font-size: 2.3em;
+         justify-content: center;
+         display: flex;
+         margin: 60px;
+         font-weight: bold;
+         cursor: pointer;
+         transition: .5s ease-in-out;
+     }
+
+     .main input {
+         width: 60%;
+         height: 40px;
+         background: #e0dede;
+         justify-content: center;
+         display: flex;
+         margin: 20px auto;
+         padding: 10px;
+         border: none;
+         outline: none;
+         border-radius: 5px;
+     }
+
+     .main button {
+         width: 60%;
+         height: 40px;
+         margin: 10px auto;
+         justify-content: center;
+         display: block;
+         color: #fff;
+         background: #f48840;
+         font-size: 1em;
+         font-weight: bold;
+         margin-top: 20px;
+         outline: none;
+         border: none;
+         border-radius: 5px;
+         transition: .2s ease-in;
+         cursor: pointer;
+     }
+
+     .main button:hover {
+         background: #f48840ed;
+     }
+
+     .login {
+         height: 460px;
+         background: #eee;
+         border-radius: 60% / 10%;
+         transform: translateY(-180px);
+         transition: .8s ease-in-out;
+     }
+
+     .login label {
+         color: #f48840;
+         transform: scale(.6);
+     }
+
+     #chk:checked~.login {
+         transform: translateY(-500px);
+     }
+
+     #chk:checked~.login label {
+         transform: scale(1);
+     }
+
+     #chk:checked~.signup label {
+         transform: scale(.6);
+     }
+ </style>
+
+
+ <script>
+     document.getElementById('close').addEventListener('click', function() {
+         document.getElementsByClassName('model-tog')[0].style.display = "none";
+     });
+     document.getElementById('show').addEventListener('click', function() {
+         document.getElementsByClassName('model-tog')[0].style.display = "flex";
+     });
+ </script>
